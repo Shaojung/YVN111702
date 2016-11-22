791d2e00c888472bb39ea2b5dd272ba5d1723350
shaojung <shaojung@gmail.com>
2016-11-22 19:01:26 +0800

    add Preference Activity

--- app/src/main/java/com/example/yvtc/yvn111702/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn111702/MainActivity.java
@@ -1,30 +1,38 @@
 package com.example.yvtc.yvn111702;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sp;
+    EditText ed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ed = (EditText) findViewById(R.id.editText);
         sp = getSharedPreferences("mydata", MODE_PRIVATE);
     }
 
     public void click1(View v)
     {
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString("name", "Teacher");
+        editor.putString("name", ed.getText().toString());
         editor.commit();
     }
     public void click2(View v)
     {
         String str = sp.getString("name", "Test");
         Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+    }
+    public void clickSettings(View v)
+    {
+        Intent it = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(it);
     }
 }
